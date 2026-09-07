5c5013f1ec35b9138c4d142b645f1984931ab0b4
Vikas V Kulkarni <kulkarnivikki@gmail.com>
2026-09-07 17:37:37 +0530

    Update apb4_sequencer.sv

--- uvm/apb4_agent/apb4_sequencer.sv
+++ uvm/apb4_agent/apb4_sequencer.sv
@@ -1,1 +1,7 @@
-.gitlookup
+class apb4_sequencer extends uvm_sequencer #(apb4_transaction);
+    `uvm_component_utils(apb4_sequencer)
+
+    function new(string name = "apb4_sequencer", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+endclass
