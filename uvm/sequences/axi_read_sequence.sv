b707c97300627d37dcf912871546958e935995b8
Vikas V Kulkarni <kulkarnivikki@gmail.com>
2026-09-07 17:43:10 +0530

    Update axi_read_sequence.sv

--- uvm/sequences/axi_read_sequence.sv
+++ uvm/sequences/axi_read_sequence.sv
@@ -1,1 +1,12 @@
-.gitlookup
+class axi_read_sequence extends axi_base_sequence;
+    `uvm_object_utils(axi_read_sequence)
+
+    function new(string name = "axi_read_sequence");
+        super.new(name);
+    endfunction
+
+    task body();
+        send_read(32'h0000_0010, 3'b000);
+        send_read(32'h0000_0020, 3'b000);
+    endtask
+endclass
