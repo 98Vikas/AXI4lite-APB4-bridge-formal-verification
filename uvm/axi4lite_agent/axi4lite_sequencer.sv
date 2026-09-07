957207b08cc91cf92ba007f115eec519a4a59f4c
Vikas V Kulkarni <kulkarnivikki@gmail.com>
2026-09-07 17:39:44 +0530

    Update axi4lite_sequencer.sv

--- uvm/axi4lite_agent/axi4lite_sequencer.sv
+++ uvm/axi4lite_agent/axi4lite_sequencer.sv
@@ -1,1 +1,7 @@
-.gitlookup
+class axi4lite_sequencer extends uvm_sequencer #(axi4lite_transaction);
+    `uvm_component_utils(axi4lite_sequencer)
+
+    function new(string name = "axi4lite_sequencer", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+endclass
